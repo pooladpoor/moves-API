78d38a04a4b3f37e9446749a0ac2b0ae566ecc92
pooladpoor <pwladpwr@gmail.com>
2024-09-13 13:56:37 +0330

    create movie model and add meny py file

--- api_moves/models.py
+++ api_moves/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Movie(models.Model):
+    Poster_Link = models.URLField(max_length=200)
+    Series_Title = models.CharField(max_length=70)
+    Released_Year = models.ImageField()
+    Certificate = models.CharField(max_length=10)
+    Runtime = models.CharField(max_length=10)
+    Genre = models.CharField(max_length=40)
+    IMDB_Rating = models.FloatField()
+    Overview = models.TextField()
+    Meta_score = models.IntegerField(null=True, blank=True)
+    Director = models.CharField(max_length=40)
+    Stars = models.CharField(max_length=100)
+    Gross = models.BigIntegerField()
+
