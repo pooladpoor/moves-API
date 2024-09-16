41686927d05f92ebf067b6920094ba25e0e38f78
pooladpoor <pwladpwr@gmail.com>
2024-09-16 11:31:14 +0330

    add doc for swagger

--- api_moves/views.py
+++ api_moves/views.py
@@ -12,6 +12,10 @@ from .serializers import MovieSerializer
 class MoviesListApiView(APIView):
 
     def get(self, request: Request):
+        """
+        get all movies (5 on each page) \n
+        The link of the previous and next page is in JSON
+        """
         movies = Movie.objects.all()
         paginator = PageNumberPagination()
         paginator.page_size = 5  # تعداد آیتم هر صفحه
@@ -20,7 +24,10 @@ class MoviesListApiView(APIView):
         return paginator.get_paginated_response(serializer.data)
     
     def post(self, request: Request):
-        
+        """
+        add movie \n
+        In the body of the request, put a json with the format of jsons received from the server
+        """
         serializer = MovieSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -35,11 +42,18 @@ class MoviesDetailApiView(APIView):
         return get_object_or_404(Movie, pk=movie_id)
 
     def get(self, request: Request, movie_id: int):
+        """
+        get movie
+        """
         movie = self.get_object(movie_id)
         serializer = MovieSerializer(movie)
         return Response(serializer.data, status.HTTP_200_OK)
     
     def put(self, request: Request, movie_id: int):
+        """
+        edit movie \n
+        In the body of the request, put a json with the format of jsons received from the server
+        """
         movie = self.get_object(movie_id)
         serializer = MovieSerializer(movie, data=request.data)
         if serializer.is_valid():
@@ -48,6 +62,9 @@ class MoviesDetailApiView(APIView):
         return Response(None, status.HTTP_400_BAD_REQUEST)
     
     def delete(self, request: Request, movie_id: int):
+        """
+        del movie
+        """
         movie = self.get_object(movie_id)
         movie.delete()
         return Response(None, status.HTTP_204_NO_CONTENT)
